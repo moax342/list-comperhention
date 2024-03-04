8f91abb2372109cbeda66621a3ac1df7aeb955a0
Mohammed Altoum <63297751+moax342@users.noreply.github.com>
2024-03-04 11:09:16 +0200

    Update NatoListComperhstion.py

--- NatoListComperhstion.py
+++ NatoListComperhstion.py
@@ -5,7 +5,5 @@ df = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_alphabets={row.letter:row.code for (index, row) in df.iterrows()}
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 name = input("Enter your name or the name you want").upper()
-for letter in name:
-    for key in nato_alphabets:
-        if key == letter:
-            print(f"{key}:{nato_alphabets[key]}\n")
+output = {letter:nato_alphabets[letter] for letter in name}
+print(output)
